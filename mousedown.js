3e7817f5d4632cef7bc1a95fc54442b34b4bf8f5
raeima <30875444+raeima@users.noreply.github.com>
2017-09-27 08:54:22 -1000

    mouseover demo
    show div box and mouseover counter

---
+++ mousedown.js
@@ -0,0 +1,9 @@
+var i = 0;
+
+function runCounter() {
+  i++;
+  document.getElementById('theCount').innerHTML = i;
+}
+function showAnswer() {
+  document.getElementById('daAnsa').style.visibility = "visible"
+}
